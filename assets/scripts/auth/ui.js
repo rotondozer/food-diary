3951525580a0095ef040f5fe2863af532f0613ab
rotondozer <nickrotondo724@gmail.com>
2017-08-25 16:22:05 -0400

    Creates basic form fields for signing in

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,45 @@
+'use strict'
+// remove signIn and signOut
+const app = require('../app.js')
+
+// remove me before code-along
+const signInSuccess = (data) => {
+  app.user = data.user
+  console.log(app)
+  console.log('sign in success!')
+  $('#signedInUser').text(data.user.email)
+}
+
+// remove me before code-along
+const signOutSuccess = () => {
+  app.user = null
+  console.log(app)
+  console.log('signed out')
+  $('#signedInUser').text('')
+}
+
+const changePasswordSuccess = () => {
+  console.log('Password Successfully Changed.')
+}
+
+// I made this
+const signUpSuccess = (data) => {
+  console.log(data)
+}
+
+const success = (data) => {
+  console.log(data)
+}
+
+const failure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  failure,
+  success,
+  signInSuccess,
+  signOutSuccess,
+  changePasswordSuccess,
+  signUpSuccess
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://food-diary-api.herokuapp.com/'
   }
 }
 
